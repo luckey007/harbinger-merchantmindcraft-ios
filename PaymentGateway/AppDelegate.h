055ba1adf62bc1142f664df99d8d51ef073f7707
qtest1982+ios@gmail.com <qtest1982+ios@gmail.com>
2015-11-28 11:37:31 +0530

    Initial Commit

---
+++ PaymentGateway/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PaymentGateway
+//
+//  Created by Apple on 28/11/15.
+//  Copyright © 2015 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PaymentGateway/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PaymentGateway
+//
+//  Created by Apple on 28/11/15.
+//  Copyright © 2015 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
